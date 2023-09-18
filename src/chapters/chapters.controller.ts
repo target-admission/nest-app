81320abea0c135594f9c95001187cb55215473d3
Moon2340 <farjanamoon20144@gmail.com>
2023-09-19 03:39:51 +0600

    jani na ki bug

--- src/chapters/chapters.controller.ts
+++ src/chapters/chapters.controller.ts
@@ -1,20 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  Query,
+} from '@nestjs/common';
 import { ChaptersService } from './chapters.service';
 import { CreateChapterDto } from './dto/create-chapter.dto';
 import { UpdateChapterDto } from './dto/update-chapter.dto';
 
+import {
+  IPaginationQuery,
+  LimitQuery,
+  PageQuery,
+  SearchQuery,
+  ShowParanoidQuery,
+  SortQuery,
+  TrashQuery,
+} from 'src/utils/Pagination/dto/query.dto';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('chapter')
 @Controller('chapters')
 export class ChaptersController {
   constructor(private readonly chaptersService: ChaptersService) {}
 
   @Post()
+  @HttpCode(201)
   create(@Body() createChapterDto: CreateChapterDto) {
     return this.chaptersService.create(createChapterDto);
   }
 
   @Get()
-  findAll() {
-    return this.chaptersService.findAll();
+  @ApiQuery(TrashQuery)
+  @ApiQuery(ShowParanoidQuery)
+  @ApiQuery(SortQuery)
+  @ApiQuery(PageQuery)
+  @ApiQuery(LimitQuery)
+  @ApiQuery(SearchQuery)
+  findAll(@Query() query: IPaginationQuery) {
+    return this.chaptersService.findAll(query);
   }
 
   @Get(':id')
@@ -28,7 +57,21 @@ export class ChaptersController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.chaptersService.remove(+id);
+  @ApiQuery({
+    name: 'permanent',
+    type: 'boolean',
+    required: false,
+  })
+  @ApiQuery({
+    name: 'restore',
+    type: 'boolean',
+    required: false,
+  })
+  remove(
+    @Param('id') id: string,
+    @Query('permanent') permanent?: boolean,
+    @Query('restore') restore?: boolean,
+  ) {
+    return this.chaptersService.remove(+id, permanent, restore);
   }
 }
--- src/chapters/chapters.service.ts
+++ src/chapters/chapters.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateChapterDto } from './dto/create-chapter.dto';
 import { UpdateChapterDto } from './dto/update-chapter.dto';
 import Chapter from './entities/chapter.entity';
@@ -9,24 +13,96 @@ import { Op } from 'sequelize';
 @Injectable()
 export class ChaptersService {
   async create(createChapterDto: CreateChapterDto) {
-    const {} = CreateChapterDto;
+    const { chapter_name, description } = CreateChapterDto;
 
-    await Chapter.create({});
+    await Chapter.create({
+      chapter_name,
+      description,
+    });
+    return {
+      statusCode: 201,
+      message: `${chapter_name} registered as a chapter successfully`,
+    };
   }
 
-  findAll() {
-    return `This action returns all chapters`;
+  async findAll(query: IPaginationQuery, _chapter_name?: string) {
+    const pagination = new Pagination(query);
+
+    const { limit, offset, paranoid, trash_query } =
+      pagination.get_attributes();
+
+    const search_ops = pagination.get_search_ops(['chapter_name']);
+    return pagination.arrange(
+      await Chapter.findAndCountAll({
+        where: {
+          [Op.or]: search_ops,
+          ...trash_query,
+        },
+        paranoid,
+        limit,
+        offset,
+      }),
+    );
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} chapter`;
+  async findOne(id: number) {
+    const chapter = await Chapter.findByPk(id, {
+      paranoid: false,
+    });
+
+    if (!chapter) {
+      throw new NotFoundException(`Chapter not found`);
+    }
+    return {
+      message: 'Chapter fetched successfully',
+      data: chapter,
+    };
   }
 
-  update(id: number, updateChapterDto: UpdateChapterDto) {
-    return `This action updates a #${id} chapter`;
+  async update(id: number, updateChapterDto: UpdateChapterDto) {
+    const { chapter_name, description } = updateChapterDto;
+
+    const chapter = await Chapter.findByPk(id);
+    if (!chapter) {
+      throw new NotFoundException(`Chapter not found`);
+    }
+    await chapter.update({
+      chapter_name,
+      description,
+    });
+    return {
+      message: 'Chapter updated successfully',
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} chapter`;
+  async remove(id: number, permanent?: boolean, restore?: boolean) {
+    const chapter = await Chapter.findByPk(id, {
+      paranoid: false,
+    });
+
+    if (!chapter) {
+      throw new NotFoundException(`Chapter not found`);
+    }
+
+    if (permanent) {
+      await chapter.destroy({ force: true });
+      return {
+        message: 'Chapter deleted permanently',
+      };
+    } else if (restore) {
+      if (chapter.deleted_at === null) {
+        throw new BadRequestException(`Chapter not deleted`);
+      }
+      chapter.restore();
+      return {
+        message: 'Chapter restored successfully',
+      };
+    }
+
+    await chapter.destroy();
+
+    return {
+      message: 'chapter deleted successfully',
+    };
   }
 }
