7098bb7638f1932386166119586f04d3e91374a1
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-23 16:03:11 +0600

    added: answer

--- src/answers/answers.controller.ts
+++ src/answers/answers.controller.ts
@@ -6,11 +6,23 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { AnswersService } from './answers.service';
 import { CreateAnswerDto } from './dto/create-answer.dto';
 import { UpdateAnswerDto } from './dto/update-answer.dto';
+import {
+  IPaginationQuery,
+  LimitQuery,
+  PageQuery,
+  SearchQuery,
+  ShowParanoidQuery,
+  SortQuery,
+  TrashQuery,
+} from 'src/utils/Pagination/dto/query.dto';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Answers')
 @Controller('answers')
 export class AnswersController {
   constructor(private readonly answersService: AnswersService) {}
@@ -21,8 +33,24 @@ export class AnswersController {
   }
 
   @Get()
-  findAll() {
-    return this.answersService.findAll();
+  @ApiQuery({
+    name: 'question_id',
+    type: 'number',
+    required: false,
+  })
+
+  // Pagination Queries
+  @ApiQuery(TrashQuery)
+  @ApiQuery(ShowParanoidQuery)
+  @ApiQuery(SortQuery)
+  @ApiQuery(PageQuery)
+  @ApiQuery(LimitQuery)
+  @ApiQuery(SearchQuery)
+  findAll(
+    @Query() query: IPaginationQuery,
+    @Query('question_id') question_id?: number,
+  ) {
+    return this.answersService.findAll(query, question_id);
   }
 
   @Get(':id')
@@ -36,7 +64,21 @@ export class AnswersController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.answersService.remove(+id);
+  @ApiQuery({
+    name: 'permanent',
+    type: 'boolean',
+    required: false,
+  })
+  @ApiQuery({
+    name: 'restore',
+    type: 'boolean',
+    required: false,
+  })
+  remove(
+    @Param('id') id: string,
+    @Query('permanent') permanent?: boolean,
+    @Query('restore') restore?: boolean,
+  ) {
+    return this.answersService.remove(+id, permanent, restore);
   }
 }
--- src/answers/answers.service.ts
+++ src/answers/answers.service.ts
@@ -1,26 +1,141 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateAnswerDto } from './dto/create-answer.dto';
 import { UpdateAnswerDto } from './dto/update-answer.dto';
+import { IPaginationQuery } from 'src/utils/Pagination/dto/query.dto';
+import Pagination from 'src/utils/Pagination';
+import Answer from './entities/answer.entity';
+import { Op } from 'sequelize';
+import Question from 'src/questions/entities/question.entity';
 
 @Injectable()
 export class AnswersService {
-  create(createAnswerDto: CreateAnswerDto) {
-    return 'This action adds a new answer';
+  async create(createAnswerDto: CreateAnswerDto) {
+    try {
+      const { title, attachment, question_id } = createAnswerDto;
+      // Create question bank
+
+      const answer = await Answer.create({
+        title,
+        attachment,
+        question_id,
+      });
+
+      return {
+        success: true,
+        data: answer.dataValues,
+        message: 'Answer created successfully',
+      };
+    } catch (error) {
+      throw new BadRequestException(
+        error?.errors?.[0]?.message || error?.message || error,
+      );
+    }
   }
 
-  findAll() {
-    return `This action returns all answers`;
+  async findAll(query: IPaginationQuery, question_id?: number) {
+    const pagination = new Pagination(query);
+
+    const { limit, offset, paranoid, trash_query, order } =
+      pagination.get_attributes();
+
+    const search_ops = pagination.get_search_ops(['title']);
+
+    const filters = pagination.format_filters({
+      question_id,
+    });
+
+    return pagination.arrange(
+      await Answer.findAndCountAll({
+        where: {
+          [Op.or]: search_ops,
+          ...filters,
+          ...trash_query,
+        },
+        order,
+        paranoid,
+        limit,
+        offset,
+      }),
+    );
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} answer`;
+  async findOne(id: number) {
+    const answer = await Answer.findByPk(id, {
+      paranoid: false,
+      include: [
+        {
+          model: Question,
+        },
+      ],
+    });
+
+    if (!answer) {
+      throw new NotFoundException(`Answer not found`);
+    }
+    return {
+      message: 'Answer fetched successfully',
+      data: answer,
+    };
   }
 
-  update(id: number, updateAnswerDto: UpdateAnswerDto) {
-    return `This action updates a #${id} answer`;
+  async update(id: number, updateAnswerDto: UpdateAnswerDto) {
+    try {
+      const { title, attachment, question_id } = updateAnswerDto;
+
+      const answer = await Answer.findByPk(id);
+
+      if (!answer) {
+        throw new NotFoundException(`Answer not found`);
+      }
+
+      await answer.update({
+        title,
+        attachment,
+        question_id,
+      });
+
+      return {
+        message: 'Answer updated successfully',
+      };
+    } catch (error) {
+      throw new BadRequestException(
+        error?.errors?.[0]?.message || error?.message || error,
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} answer`;
+  async remove(id: number, permanent?: boolean, restore?: boolean) {
+    const answer = await Answer.findByPk(id, {
+      paranoid: false,
+    });
+
+    if (!answer) {
+      throw new NotFoundException(`Answer not found`);
+    }
+
+    if (permanent) {
+      await answer.destroy({ force: true });
+      return {
+        message: 'Answer deleted permanently',
+      };
+    } else if (restore) {
+      if (answer.deleted_at === null) {
+        throw new BadRequestException(`Answer not deleted`);
+      }
+      answer.restore();
+      return {
+        message: 'Answer restored successfully',
+      };
+    }
+
+    await answer.destroy();
+
+    return {
+      message: 'Answer deleted successfully',
+    };
   }
 }
